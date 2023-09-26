a6181ffdde2dcb3b322c76c827cca79570c1151b
Medfkgit <141320850+Medfkgit@users.noreply.github.com>
2023-09-26 15:19:13 +0200

    Add files via upload

---
+++ day26.js
@@ -0,0 +1,18 @@
+
+
+
+const express = require('express');
+
+
+const app = express();
+
+
+app.get('/', (req, res) => {
+  res.send('Welcome to my Express.js server!');
+});
+
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
